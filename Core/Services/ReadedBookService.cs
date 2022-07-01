cdaf42d48e36a5b744e2ee071b1bac0f64e48948
Gabriela Gondek <ggondek01@gmail.com>
2022-07-01 12:53:59 +0200

    Add ReadedBookService logic and interface

--- Core/Services/ReadedBookService.cs
+++ Core/Services/ReadedBookService.cs
@@ -1,6 +1,56 @@
-﻿namespace Core.Services;
+﻿using Core.Entities;
 
-public class ReadedBookService
+namespace Core.Services;
+
+public interface IReadedBookService
 {
+    void Create(string title,sbyte rate);
+    void Update(int id, string title,sbyte rate);
+    void Delete(int id);
+    ReadedBook GetById(int id);
+    List<ReadedBook> GetAll();
+}
+
+public class ReadedBookService : IReadedBookService
+{
+    private readonly AppDbContext _context;
     
+    public ReadedBookService()
+    {
+        _context = new ContextFactory().CreateDbContext();
+    }
+
+    public void Create(string title,sbyte rate)
+    {
+        _context.ReadedBooks.Add(new ReadedBook() {Title = title,Rate = rate });
+    }
+
+    public void Update(int id, string title,sbyte rate)
+    {
+        var book = _context.ReadedBooks.FirstOrDefault(x => x.Id == id);
+        if (book == null) throw new Exception("Null reference");
+        book.Rate = rate;
+        book.Title = title;
+        _context.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+        var book = _context.ReadedBooks.FirstOrDefault(x => x.Id == id);
+        if (book == null) throw new Exception("Null reference");
+        _context.ReadedBooks.Remove(book);
+    }
+
+    public ReadedBook GetById(int id)
+    {
+        var book = _context.ReadedBooks.FirstOrDefault(x => x.Id == id);
+        if (book == null) throw new Exception("Null reference");
+        return book;
+    }
+
+    public List<ReadedBook> GetAll()
+    {
+        var books = _context.ReadedBooks.ToList();
+        return books;
+    }
 }
