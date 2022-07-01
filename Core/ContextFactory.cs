47f63f7c41538d0d4a13a23fba42ef21fd55ccdb
Gabriela Gondek <ggondek01@gmail.com>
2022-07-01 12:26:35 +0200

    Add ContextFactory class and empty services

---
+++ Core/ContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Core;
+
+public class ContextFactory:IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args = null)
+    {
+        var opt = new DbContextOptionsBuilder<AppDbContext>();
+        opt.UseSqlServer("Server=.; Database=Library; Trusted_Connection=True");
+        return new AppDbContext(opt.Options);
+    }
+}
---
+++ Core/Services/BookToReadService.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Services;
+
+public class BookToReadService
+{
+    private readonly AppDbContext _context;
+    
+    public BookToReadService()
+    {
+        _context = new ContextFactory().CreateDbContext();
+    }
+    
+}
