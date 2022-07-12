a197e205f9e2096c4a43edd28eba57342461e09d
Gabriela Gondek <ggondek01@gmail.com>
2022-07-12 16:55:40 +0200

    Migration

---
+++ Core/Migrations/20220712145328_EntitiesUpdate.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    public partial class EntitiesUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReadedBooks");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "BooksToRead",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Pages",
+                table: "BooksToRead",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PublicationDate",
+                table: "BooksToRead",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Publisher",
+                table: "BooksToRead",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "BooksToBuy",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Pages",
+                table: "BooksToBuy",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PublicationDate",
+                table: "BooksToBuy",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Publisher",
+                table: "BooksToBuy",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "ReadBooks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rate = table.Column<short>(type: "smallint", nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Publisher = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PublicationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Pages = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReadBooks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReadBooks");
+
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "BooksToRead");
+
+            migrationBuilder.DropColumn(
+                name: "Pages",
+                table: "BooksToRead");
+
+            migrationBuilder.DropColumn(
+                name: "PublicationDate",
+                table: "BooksToRead");
+
+            migrationBuilder.DropColumn(
+                name: "Publisher",
+                table: "BooksToRead");
+
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "BooksToBuy");
+
+            migrationBuilder.DropColumn(
+                name: "Pages",
+                table: "BooksToBuy");
+
+            migrationBuilder.DropColumn(
+                name: "PublicationDate",
+                table: "BooksToBuy");
+
+            migrationBuilder.DropColumn(
+                name: "Publisher",
+                table: "BooksToBuy");
+
+            migrationBuilder.CreateTable(
+                name: "ReadedBooks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Rate = table.Column<short>(type: "smallint", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReadedBooks", x => x.Id);
+                });
+        }
+    }
+}
